48a86cebc496dd4eb1bc0ddf11e9dc446fd436da
Alumno <Alumno@PC78>
2026-04-21 09:58:50 -0300

    Agregar archivos de proyecto.

---
+++ PryBarreraFix/clsConexionDB.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Data.OleDb;
+
+namespace PryBarreraFix
+{
+    public class clsConexionDB
+    {
+        // Conexión y estado
+        private OleDbConnection conexion;
+        public string estadoConexion { get; private set; }
+
+        public void conectarBaseDeDatos()
+        {
+            try
+            {
+                // Ruta relativa a la carpeta BaseDeDatos dentro del proyecto
+                string ruta = AppDomain.CurrentDomain.BaseDirectory
+                              + @"BaseDeDatos\EL_CLUB.accdb";
+
+                string cadenaConexion =
+                    $"Provider=Microsoft.ACE.OLEDB.12.0;Data Source={ruta};Persist Security Info=False;";
+
+                conexion = new OleDbConnection(cadenaConexion);
+                conexion.Open();
+
+                estadoConexion = "✅ Conexión exitosa";
+            }
+            catch (Exception ex)
+            {
+                estadoConexion = "❌ Error: " + ex.Message;
+            }
+        }
+
+        // Método para obtener la conexión desde otros formularios
+        public OleDbConnection obtenerConexion()
+        {
+            return conexion;
+        }
+
+        // Cerrar conexión
+        public void cerrarConexion()
+        {
+            if (conexion != null && conexion.State == System.Data.ConnectionState.Open)
+                conexion.Close();
+        }
+    }
+}
---
+++ PryBarreraFix/frmInicio.Designer.cs
@@ -0,0 +1,92 @@
+﻿
+namespace pryFernandezIES
+{
+    partial class frmInicio
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmInicio));
+            this.btnIniciar = new System.Windows.Forms.Button();
+            this.pctInicio = new System.Windows.Forms.PictureBox();
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Sizable;
+            ((System.ComponentModel.ISupportInitialize)(this.pctInicio)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnIniciar
+            // 
+            this.btnIniciar.BackColor = System.Drawing.Color.White;
+            this.btnIniciar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnIniciar.FlatAppearance.BorderSize = 0;
+            this.btnIniciar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnIniciar.Font = new System.Drawing.Font("Gadugi", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnIniciar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(73)))), ((int)(((byte)(123)))));
+            this.btnIniciar.Location = new System.Drawing.Point(407, 269);
+            this.btnIniciar.Margin = new System.Windows.Forms.Padding(0);
+            this.btnIniciar.Name = "btnIniciar";
+            this.btnIniciar.Size = new System.Drawing.Size(171, 50);
+            this.btnIniciar.TabIndex = 1;
+            this.btnIniciar.Text = "INICIAR";
+            this.btnIniciar.UseVisualStyleBackColor = false;
+            this.btnIniciar.Click += new System.EventHandler(this.btnIniciar_Click);
+            // 
+            // pctInicio
+            // 
+            this.pctInicio.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pctInicio.Location = new System.Drawing.Point(0, 0);
+            this.pctInicio.Name = "pctInicio";
+            this.pctInicio.Size = new System.Drawing.Size(950, 600);
+            this.pctInicio.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pctInicio.TabIndex = 3;
+            this.pctInicio.TabStop = false;
+            // 
+            // frmInicio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(950, 600);
+            this.Controls.Add(this.btnIniciar);
+            this.Controls.Add(this.pctInicio);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.Name = "frmInicio";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Gestion de Ventas Seguro";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.frmInicio_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pctInicio)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnIniciar;
+        private System.Windows.Forms.PictureBox pctInicio;
+    }
+}
+
